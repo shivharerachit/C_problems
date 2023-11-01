76204c8de723690d184a5d81d26cce5db69b1fc2
Rachit Shivhare <124644552+shivharerachit@users.noreply.github.com>
2023-11-01 15:36:59 +0530

    Update linkedlist.c

--- DSA/linkedlist.c
+++ DSA/linkedlist.c
@@ -1,55 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node
-{
-	int data;
-	struct node *next;
+
+struct node{
+    int data;
+    struct node *next;
 };
+
 struct node *add;
-void createlist(int n)
-{
-	int i=0,data,value,r;
-	//r=n;
-	struct node *new1,*p;
-	for(i=0;i<5;i++)
-	{
-		new1=(struct node *)malloc(sizeof(struct node));
-		printf("Enter the value:");
-		scanf("%d",&value);
-		new1->data=value;
-		new1->next=NULL;
-		if(i=0)
-		{
-			add=new1;
-			p=new1;
-		}
-		else
-		{
-			p->next=new1;
-			p=new1;
-		}
-	}
-}
-void traverselist()
-{
-	int value;
-	struct node *new1,*p;
-	new1=add;
-	while(new1->next!=NULL)
-	{
-		value=new1->data;
-		p=new1;
-		new1=p->next;
-		printf("The value is %d.",value);
-	}
-}
-int main()
-{
-	int n;
-	//printf("enter the no. elment:");
-	//scanf("%d",&n);
-//	printf("%d",n);
-	createlist(5);
-	traverselist();
+
+void putdata(int n){
+    int value,i;
+    struct node *new1, *p;
+    for(i=0;i<n;i++){
+        printf("Enter the value:");
+        scanf("%d",&value);
+        new1=(struct node *)malloc(sizeof(struct node));
+        new1->data=value;
+        new1->next=NULL;   
+        if(i==0){
+            add=new1;
+        }
+        else{
+            p->next=new1;
+        }
+        p=new1;
+    }
 }
 
+void getdata(int n){
+    int ans,i=0;
+    struct node *new1,*p;
+    new1=add;
+    while(i!=n){
+        ans=new1->data;
+        printf("The value is %d.\n",ans);
+        new1=new1->next;
+        p=new1;
+        i++;
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter the number of terms:");
+    scanf("%d",&n);
+    putdata(n);
+    getdata(n);
+    return 0;
+}
